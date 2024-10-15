42da109440764bfd742f2f9f427ba8bc292f38f8
“inmancosta” <inman.costa1@gmail.com>
2024-10-14 20:19:39 -0700

    Initial Commit

---
+++ crux/cruxApp.swift
@@ -0,0 +1,17 @@
+//
+//  cruxApp.swift
+//  crux
+//
+//  Created by Crist Costa on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct cruxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
